fb06eb94d6ca85c38343a362cf7502d2e1172a8e
timothy <pimtarka@gmail.com>
2023-04-10 18:17:15 -0400

    finished lab

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -36,4 +38,77 @@ public class HamletParser {
         return hamletData;
     }
 
+    public void changeHamletToLeon() {
+        Pattern pattern = Pattern.compile("Hamlet");
+        Matcher matcher = pattern.matcher(hamletData);
+        StringBuffer sb = new StringBuffer();
+        String replacementText = "Leon";
+        //String matchedText;
+        while (matcher.find()) {
+            //matchedText = matcher.group();
+            matcher.appendReplacement(sb, replacementText);
+        }
+        matcher.appendTail(sb);
+        hamletData = sb.toString();
+
+        pattern = Pattern.compile("HAMLET");
+        matcher = pattern.matcher(hamletData);
+        sb = new StringBuffer();
+        String replacementText2 = "LEON";
+        //matchedText;
+        while (matcher.find()) {
+            //matchedText = matcher.group();
+            matcher.appendReplacement(sb, replacementText2);
+        }
+        matcher.appendTail(sb);
+
+        hamletData = sb.toString();
+    }
+
+    public void changeHoratioToTariq() {
+        Pattern pattern = Pattern.compile("Horatio");
+        Matcher matcher = pattern.matcher(hamletData);
+        StringBuffer sb = new StringBuffer();
+        String replacementText = "Tariq";
+        //String matchedText;
+        while (matcher.find()) {
+            //matchedText = matcher.group();
+            matcher.appendReplacement(sb, replacementText);
+        }
+        matcher.appendTail(sb);
+        hamletData = sb.toString();
+
+        pattern = Pattern.compile("HORATIO");
+        matcher = pattern.matcher(hamletData);
+        sb = new StringBuffer();
+        String replacementText2 = "TARIQ";
+        //matchedText;
+        while (matcher.find()) {
+            //matchedText = matcher.group();
+            matcher.appendReplacement(sb, replacementText2);
+        }
+        matcher.appendTail(sb);
+
+        hamletData = sb.toString();
+    }
+
+    public int findHoratio() {
+        Pattern pattern = Pattern.compile("Horatio", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(hamletData);
+        int count = 0;
+        while (matcher.find()) {
+            count++;
+        }
+        return count;
+    }
+
+    public int findHamlet() {
+        Pattern pattern = Pattern.compile("Hamlet", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(hamletData);
+        int count = 0;
+        while (matcher.find()) {
+            count++;
+        }
+        return count;
+    }
 }
